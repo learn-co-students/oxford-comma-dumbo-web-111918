7bb3f7be70becbc11191c057c68f33a3b19f171c
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-17 20:53:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length >= 3
+    last = "and #{array[-1]}"
+    array.pop
+    array.push(last)
+    return array.join(", ")
+  end
 end
+
+
