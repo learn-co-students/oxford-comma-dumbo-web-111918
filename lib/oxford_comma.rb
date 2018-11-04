77abdc1201d1b5807a2ae75518084cc602abace6
Joseph <joeephus@gmail.com>
2018-11-04 13:59:50 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
-
+arr = ["kiwi", "Durian", "Starfruit"]
+def oxford_comma(arr)
+	if arr.length == 1
+		return arr.join
+    elsif arr.length == 2
+    return arr.join(" and ")
+    else 
+      last_poss = []
+      i = -1
+      while i < arr.length - 2
+      i += 1 
+      last_poss.push(arr[i])
+      end
+      last_poss.join(", ") + ", and " + (arr[-1]).to_s
+	end	
 end
